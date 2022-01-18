742d8e6f1de62b9d5379ff5362957182689f1ea1
Jener Hernan <74436720+JenerHernan@users.noreply.github.com>
2022-01-18 11:05:12 +0100

    Ejecutable acabado

--- reus_act9/src/reus_act9_1/Main.java
+++ reus_act9/src/reus_act9_1/Main.java
@@ -1,5 +1,6 @@
 package reus_act9_1;
 
+
 import javax.swing.JOptionPane;
 
 
@@ -8,12 +9,35 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Electrodomestico e1 = new Electrodomestico();	
-		Electrodomestico e2 = new Electrodomestico(200,400);	
-
-		System.out.println(e1.toString());
-		System.out.println(e2.toString());
-		CrearElectrodomestico.Crear();
+		double preciosE = 0, preciosL = 0, preciosT = 0; 
 		
+		Electrodomestico electrodomesticos[] = new Electrodomestico [10];
+		electrodomesticos[0] = new Electrodomestico();
+		electrodomesticos[1] = new Electrodomestico(20,Consumo.A,Colores.azul,20);
+		electrodomesticos[2] = new Lavadora(10, 50, 10, Consumo.C, Colores.azul);
+		electrodomesticos[3] = new Television(200, Consumo.C, Colores.azul, 20, 2000, true);
+		electrodomesticos[4] = new Lavadora(10, 50, 10, Consumo.C, Colores.azul);
+		electrodomesticos[5] = new Electrodomestico();
+		electrodomesticos[6] = new Electrodomestico(20,Consumo.A,Colores.azul,20);
+		electrodomesticos[7] = new Lavadora(10, 50, 10, Consumo.C, Colores.azul);
+		electrodomesticos[8] = new Television(200, Consumo.C, Colores.azul, 20, 2000, true);
+		electrodomesticos[9] = new Lavadora(10, 50, 10, Consumo.C, Colores.azul);
+		
+	
+		
+		for(int i = 0; i < 10; i++) {
+			electrodomesticos[i].setPreciob(electrodomesticos[i].PrecioFinal(electrodomesticos[i].getPeso(), electrodomesticos[i].getConsumo()));
+			
+			if(electrodomesticos[i] instanceof Television)
+				preciosT += electrodomesticos[i].getPreciob();
+			else
+				preciosL += electrodomesticos[i].getPreciob();
+			
+			preciosE += electrodomesticos[i].getPreciob();
+				
+			
+		}
+		
+		System.out.println("El precio total de todos los electrodomesticos es " + preciosE + ", el precio de todas las lavadoras es " + preciosL + ", el precio total de todas las TVs es " + preciosT);
 	}
 }
